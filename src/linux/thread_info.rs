fb382c0aa9a14dbbf571d06eae12c22ecab090ba
helce <helce@bk.ru>
2026-09-03 16:09:16 +0300

    using ptrace_getregs_data instead

--- src/linux/thread_info.rs
+++ src/linux/thread_info.rs
@@ -95,6 +95,7 @@ trait CommonThreadInfo {
     /// Some ptrace get requests populate structs or larger elements than `c_long`
     /// and therefore use the data field to return values. This function handles these
     /// requests.
+    #[cfg(not(target_arch = "e2k"))]
     fn ptrace_get_data<T>(
         request: ptrace::RequestType,
         flag: Option<NT_Elf>,
